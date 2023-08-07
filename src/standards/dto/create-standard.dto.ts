446c2fc93e96a47cde0e3df6585b8d46ce1fe26a
shinekumar23 <shine.kumar@epiuse.com>
2023-08-07 20:46:06 +0530

    test

--- src/standards/dto/create-standard.dto.ts
+++
@@ -1,9 +0,0 @@
-export class CreateStandardDto {
-    uniqueid?: string;
-    nativeid: string;
-    description: string;
-    isleaf: boolean;
-    createdby: string;
-    refstatustypeid: number;
-    frameworkid: number;
-  }
--- src/standards/dto/update-standard.dto.ts
+++
@@ -1,9 +0,0 @@
-export class UpdateStandardDto {
-    uniqueid?: string;
-    nativeid?: string;
-    notes?: string;
-    isleaf?: boolean;
-    updatedby: string;
-    refstatustypeid?: number;
-    frameworkid?: number;
-  }
